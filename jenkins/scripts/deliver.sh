de5e390f18eb5f5f388a137cceaa7c6d576eb081
vasanthagokuls-virtusa <vasanthagokuls@virtusa.com>
2024-07-05 00:41:14 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -8,19 +8,20 @@ set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
-echo 'The following command extracts the value of the <name/> element'
+echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=$(mvn help:evaluate -Dexpression=project.name | grep -v "^\[" | sed 's/\x1b\[[0-9;]*m//g')
 set +x
 
-echo 'The following command behaves similarly to the previous one but'
+echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep -E "^[0-9]+\.[0-9]+.*"`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
+
 java -jar target/${NAME}-${VERSION}.jar
